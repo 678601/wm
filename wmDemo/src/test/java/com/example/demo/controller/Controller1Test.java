47b0688a5b416dcb74f2aaaa3b0d5e57834733c8
lwm <381185864@qq.com>
2020-12-27 08:31:56 +0800

    代码梳理

--- wmDemo/src/test/java/com/example/demo/controller/Controller1Test.java
+++ wmDemo/src/test/java/com/example/demo/controller/Controller1Test.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-import static org.junit.Assert.*;
-
+import com.example.demo.WmDemoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.demo.WmDemoApplication;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(WmDemoApplication.class)
